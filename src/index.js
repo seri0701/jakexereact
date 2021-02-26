d86a78dade26b1c4be8bfb6f166e0afb2160366d
seri0701 <76086657+seri0701@users.noreply.github.com>
2021-02-27 07:46:31 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// import React from "react";jsxを使用するだけならimport不要
+import ReactDom from "react-dom";
+
+const App = () => {
+  return null;
+};
+
+ReactDom.render(<App />, document.getElementById("root"));
